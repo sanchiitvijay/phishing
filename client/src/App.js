c95cbeab043c85b2ca31a38bfc6a097157d66aab
Sanchiitvjay <sanchiitvjay@gmail.com>
2024-07-16 10:24:53 +0530

    addded some css

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ const App = () => {
     event.preventDefault();
     console.log({url});
     setIsLoading(true);
+    setResult('');
     const payload = { url };
     fetch('http://127.0.0.1:5000/predict', {
       method: 'POST',
@@ -36,14 +37,24 @@ const App = () => {
   },[isLoading])
 
   return (
-    <div >
+    <div>
       <a href="https://www.github.com/" className='right-8 absolute' >
           <FaGithub size={30} color={"white"}/>
           </a>
-      <div className="heading flex flex-col md:mt-12 mt-10 font-semibold text-center m-6 h-[70px] md:text-5xl text-3xl bg-gradient-to-r from-green-900 via-blue-400 to-indigo-400 text-transparent bg-clip-text">
+      {/* <div className="heading flex flex-col md:mt-12 mt-10 font-semibold text-center m-6 h-[70px] md:text-5xl text-3xl bg-gradient-to-r from-green-900 via-blue-400 to-indigo-400 text-transparent bg-clip-text">
         
           Phishing Predictor
-        </div>
+        </div> */}
+        <div class="content">
+        <h1 class="title">Diabetics Predictor
+          <div class="aurora">
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+          </div>
+        </h1>
+      </div>
 
         <form id="fileForm" className='md:mt-12 mx-auto md:w-[50%] w-[90%]' onSubmit={handleSubmit}>
             <label className="input input-bordered rounded-xl max-md:text-sm flex items-center gap-3 pr-0 py-1">
